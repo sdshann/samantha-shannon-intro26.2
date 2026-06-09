e26b923d12c9cc453ff61e96104674cf17b01569
Samantha <sdshann@gmail.com>
2026-06-09 17:32:44 -0400

    open API page added

---
+++ js/openapi.js
@@ -0,0 +1,41 @@
+
+
+const imageBaseUrl = "https://www.artic.edu/iiif/2";
+
+function loadKusamaImages() {
+    fetch("https://api.artic.edu/api/v1/artworks/search?q=Yayoi+Kusama&limit=1&fields=id,title,artist_display,image_id")
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById("kusamaImages");
+            container.innerHTML = "";
+
+            data.data.forEach(artwork => {
+                const imgElement = document.createElement("img");
+                imgElement.src = `${imageBaseUrl}/${artwork.image_id}/full/843,/0/default.jpg`;
+                imgElement.alt = artwork.title;
+                container.appendChild(imgElement);
+            });
+        })
+        .catch(error => console.error('Error fetching artwork:', error));
+}
+
+document.getElementById("kusamaButton").addEventListener("click", loadKusamaImages);
+
+function loadGeorgiaImages() {
+    fetch("https://api.artic.edu/api/v1/artworks/search?q=Georgia+O'Keeffe&limit=1&fields=id,title,artist_display,image_id")
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById("georgiaImages");
+            container.innerHTML = "";
+
+            data.data.forEach(artwork => {
+                const imgElement = document.createElement("img");
+                imgElement.src = `${imageBaseUrl}/${artwork.image_id}/full/843,/0/default.jpg`;
+                imgElement.alt = artwork.title;
+                container.appendChild(imgElement);
+            });
+        })
+        .catch(error => console.error('Error fetching artwork:', error));
+}
+
+document.getElementById("georgiaButton").addEventListener("click", loadGeorgiaImages);
